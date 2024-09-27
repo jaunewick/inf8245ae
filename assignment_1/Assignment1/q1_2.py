1a4fec6acb5d7fcffcc9c518bcae4c88ae270e15
Daniel Giao <daniel.giao@icloud.com>
2024-09-27 00:05:36 -0400

    Question 1_2 code done

--- assignment_1/Assignment1/q1_2.py
+++ assignment_1/Assignment1/q1_2.py
@@ -15,5 +15,36 @@ y_test = pd.read_csv('Data/y_test.csv').values
 # Write your code here:
 
 # Find the optimal parameters only using the training set
+X_train_with_bias = data_matrix_bias(X_train)
+w_star = linear_regression_optimize(y_train, X_train_with_bias)
+
+X_test_with_bias = data_matrix_bias(X_test)
+y_hat = linear_regression_predict(X_test_with_bias, w_star)
 
 # Report the RMSE and Plot the data on the test set
+rmse_val = rmse(y_test, y_hat)
+print(f'RMSE value : {rmse_val}')
+
+experience_col = X_test[:,0]
+test_score_col = X_test[:,1]
+
+plt.figure(figsize=(12, 6))
+
+plt.subplot(1, 2, 1)
+plt.scatter(experience_col, y_test, color='blue', alpha=0.5, label='Actual Salary')
+plt.scatter(experience_col, y_hat, color='red', alpha=0.5, label='Predicted Salary')
+plt.title('Experience vs Salary')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.scatter(test_score_col, y_test, color='blue', alpha=0.5, label='Actual Salary')
+plt.scatter(test_score_col, y_hat, color='red', alpha=0.5, label='Predicted Salary')
+plt.title('Test Score vs Salary')
+plt.xlabel('Test Score')
+plt.ylabel('Salary')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
