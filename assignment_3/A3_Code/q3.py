eee12b6cc01f9c39db1f9778f713525067c1f405
Daniel Giao <daniel.giao@icloud.com>
2024-10-29 01:53:08 -0400

    q3 done

--- assignment_3/A3_Code/q3.py
+++ assignment_3/A3_Code/q3.py
@@ -10,46 +10,52 @@ from q2 import data_splits, normalize_features
 # Step 1: Create hyperparameter grids for each model
 # TODO fill out below dictionaries with reasonable values
 param_grid_decision_tree = {
-    'criterion':... ,
-    'max_depth': ...,
-    'min_samples_leaf': ...,
-    'max_leaf_nodes': ...
+    'criterion': ['gini', 'entropy', 'log_loss'],
+    'max_depth': [None, 5, 10, 20, 30, 40],
+    'min_samples_leaf': [1, 2, 4, 8, 16],
+    'max_leaf_nodes': [None, 5, 10, 20, 30, 40],
 }
 
 param_grid_random_forest = {
-    'n_estimators': ... ,
-    'max_depth': ... ,
-    'bootstrap': ... ,
+    'n_estimators': [10, 50, 100, 200],
+    'max_depth': [None, 5, 10, 15, 20],
+    'bootstrap': [True, False],
 }
 
 param_grid_svm = {
-    'kernel': ... ,
-    'shrinking': ...,
-    'C': ...,
-    'tol': ...,
-    'gamma': ...
+    'kernel': ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'],
+    'shrinking': [True, False],
+    'C': [0.1, 1, 10],
+    'tol': [1e-3, 1e-4],
+    'gamma': ['scale', 'auto'],
 }
 
 # Step 2: Initialize classifiers with random_state=0
-decision_tree = ... # TODO
-random_forest = ... # TODO
-svm = ... # TODO
+decision_tree = DecisionTreeClassifier(random_state=0)
+random_forest = RandomForestClassifier(random_state=0)
+svm = SVC(random_state=0)
 
 # Step 3: Create a scorer using F-beta score with beta=0.5
-scorer = ... # TODO
+scorer = make_scorer(fbeta_score, beta=0.5)
 
 
 # Step 4: Perform grid search for each model using 9-fold StratifiedKFold cross-validation
 def perform_grid_search(model, X_train, y_train, params):
     # Define the cross-validation strategy
-    strat_kfold = ... # TODO
+    strat_kfold = StratifiedKFold(n_splits=9, shuffle=True, random_state=0)
 
     # Grid search for the model
-    grid_search = ... # TODO
+    grid_search = GridSearchCV(
+        estimator=model,
+        param_grid=params,
+        scoring=scorer,
+        cv=strat_kfold
+    )
     # TODO fit to the data
+    grid_search.fit(X_train, y_train)
 
-    best_param = ... # TODO
-    best_score = ... # TODO
+    best_param =  grid_search.best_params_
+    best_score = grid_search.best_score_
     print("Best parameters are:", best_param)
     print("Best score is:", best_score)
 
@@ -63,13 +69,28 @@ X_train, X_test, y_train, y_test = data_splits(X, y)
 X_train_scaled, X_test_scaled = normalize_features(X_train, X_test)
 
 # Do Grid search for Decision Tree
-grid_decision_tree, best_params_decision_tree, best_score_decision_tree  = perform_grid_search(... ) # TODO
+grid_decision_tree, best_params_decision_tree, best_score_decision_tree  = perform_grid_search(
+    decision_tree,
+    X_train_scaled,
+    y_train,
+    param_grid_decision_tree
+) # TODO
 
 # Do Grid search for Random Forest
-grid_random_forest, best_params_random_forest, best_score_random_forest  = perform_grid_search(...) # TODO
+grid_random_forest, best_params_random_forest, best_score_random_forest  = perform_grid_search(
+    random_forest,
+    X_train_scaled,
+    y_train,
+    param_grid_random_forest
+) # TODO
 
 # Do Grid search for SVM
-grid_svm, best_params_svm, best_score_svm = perform_grid_search(...) # TODO
+grid_svm, best_params_svm, best_score_svm = perform_grid_search(
+    svm,
+    X_train_scaled,
+    y_train,
+    param_grid_svm
+) # TODO
 
 
 
